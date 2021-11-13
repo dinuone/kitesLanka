f20be463025e9749a990202c309e9a81ca3bb1af
dinuone <dinu11one@gmail.com>
2021-11-13 10:49:42 +0530

    test

--- app/Http/Controllers/Admin/CourseMaterialsController.php
+++ app/Http/Controllers/Admin/CourseMaterialsController.php
@@ -14,11 +14,7 @@ use File;
 
 class CourseMaterialsController extends Controller
 {
-    public function __construct()
-	{
-		$this->middleware(['auth']);
-	}
-
+   
     public function index()
     {
         $files = Material::paginate(10);
