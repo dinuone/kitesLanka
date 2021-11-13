79bc5589769ae48ed8ed2bea3a09c0cddbd5946d
dinuone <dinu11one@gmail.com>
2021-11-13 10:41:23 +0530

    check file path

--- app/Http/Controllers/Admin/CourseMaterialsController.php
+++ app/Http/Controllers/Admin/CourseMaterialsController.php
@@ -34,8 +34,8 @@ class CourseMaterialsController extends Controller
     {
         
         // return response()->download(storage_path('app/public/pdf/'.$file_name));
-        // return response()->download(storage_path('app/public/pdf/'.$file_name), null, [], null);
-        dd($file_name);
+        Storage::path('app/public/storage/pdf/'.$file_name);
+        
     }
 
     public function Removefilles($file_name)
