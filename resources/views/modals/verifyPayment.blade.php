46c2968c7e0f38b2542299e2bcc3c4fd9432720d
dinu_one <dinu11one@gmail.com>
2022-01-03 19:37:08 +0530

    image size reduced

--- resources/views/modals/verifyPayment.blade.php
+++ resources/views/modals/verifyPayment.blade.php
@@ -1,43 +1,51 @@
 <style>
-  #imgrc{width: 80%; height: 40%;}
-  p{text-align:center;}
+    #imgrc {
+        width: 1280px;
+        height: 720px;
+    }
+
+    p {
+        text-align: center;
+    }
+
 </style>
 
-<div class="modal fade verifypayment" wire:ignore.self  tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" 
-aria-hidden="true" data-keyboad="false" data-backdrop="static">
-  <div class="modal-dialog modal-dialog-centered modal-xl role="document"">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title">Verify payment</h4>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-          <form wire:submit.prevent="access">
-          @foreach ($paymentimage as $img)
-          <p>
-            <img src=" {{asset('storage/'.$img->image_path) }} " alt="recipt img" id="imgrc">
-          </p>
-          
-          @endforeach        
-          
-          <div class="form-group">
-            <label for="">Reference Number</label>
-            <input type="text" class="form-control" placeholder="Enter reference Number...." wire:model="ref_number">
-            <span class="text-danger">@error('ref_number')  {{ $message }} @enderror</span>
-          </div>
+<div class="modal fade verifypayment" wire:ignore.self tabindex="-1" role="dialog"
+    aria-labelledby="exampleModalLongTitle" aria-hidden="true" data-keyboad="false" data-backdrop="static">
+    <div class="modal-dialog modal-dialog-centered modal-xl role=" document"">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Verify payment</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form wire:submit.prevent="access">
+                    @foreach ($paymentimage as $img)
+                        <p>
+                            <img src=" {{ asset('storage/' . $img->image_path) }} " alt="recipt img" id="imgrc">
+                        </p>
 
-       </div>
+                    @endforeach
 
-          <div class="modal-footer justify-content-between">
-            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            <button type="submit" class="btn bg-indigo">Give Access</button>
-          </div>
-        </form>
-        
+                    <div class="form-group">
+                        <label for="">Reference Number</label>
+                        <input type="text" class="form-control" placeholder="Enter reference Number...."
+                            wire:model="ref_number">
+                        <span class="text-danger">@error('ref_number') {{ $message }} @enderror</span>
+                    </div>
+
+            </div>
+
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn bg-indigo">Give Access</button>
+            </div>
+            </form>
+
+        </div>
+        <!-- /.modal-content -->
     </div>
-    <!-- /.modal-content -->
-  </div>
-  <!-- /.modal-dialog -->
+    <!-- /.modal-dialog -->
 </div>
