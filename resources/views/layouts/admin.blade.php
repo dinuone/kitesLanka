be32785cbe31b76585e20db543dad7ec559302b4
dinu_one <dinu11one@gmail.com>
2022-01-19 14:26:59 +0530

    side bar dahsboard admin button

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -93,7 +93,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
                         <li class="nav-item">
                             <a href="{{ route('admin-home') }}"
-                                class="nav-link {{ request()->is('admin/home*') ? 'active' : '' }}">
+                                class="nav-link {{ request()->is('admin/dashboard*') ? 'active' : '' }}">
                                 <i class="nav-icon fas fa-home"></i>
                                 <p>Dashboard</p>
                             </a>
