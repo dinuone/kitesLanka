1ba20645b87c46a0ba1106cd969810ce703227a6
dinu_one <dinu11one@gmail.com>
2022-02-03 01:09:28 +0530

    remove popup give access btn

--- resources/views/modals/view-recipt.blade.php
+++ resources/views/modals/view-recipt.blade.php
@@ -33,7 +33,6 @@
 
             <div class="modal-footer justify-content-between">
                 <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="submit" class="btn bg-indigo">Give Access</button>
             </div>
             </form>
 
