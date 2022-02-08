6571795f11b444e15fef74d741e27847df2b7b79
dinu_one <dinu11one@gmail.com>
2022-02-08 22:18:51 +0530

    validation number amount

--- app/Http/Livewire/StudPayment.php
+++ app/Http/Livewire/StudPayment.php
@@ -61,7 +61,7 @@ class StudPayment extends Component
             'student_id'=>'required',
             'course'=>'required',
             'photo'=>'required',
-            'amount'=>'required',
+            'amount'=>'numeric|min:2|max:6',
             'month'=>'required'
             
         ]);
